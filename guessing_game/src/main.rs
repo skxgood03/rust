4df903652f9680d7ceb42258980b39427359a92c
ethan shi <ethan.shi@gempoll.com>
2024-10-25 18:55:59 +0800

    开发猜字谜例子

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+use rand::Rng;
 fn main() {
-    println!("Hello, world!");
+    println!("开始游戏!");
+    let secret_number = rand::thread_rng().gen_range(1..=101);
+    println!("随机生成：{secret_number}");
+    println!("请输出");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("无法读取行");
+    println!("输入{guess}");
 }
