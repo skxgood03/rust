6b2503940bf53e5868c16bf0369ad33e6df08353
ethan shi <ethan.shi@gempoll.com>
2024-10-28 16:17:15 +0800

    完成猜字谜游戏

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,27 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 fn main() {
     println!("开始游戏!");
     let secret_number = rand::thread_rng().gen_range(1..=101);
     println!("随机生成：{secret_number}");
-    println!("请输出");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("无法读取行");
-    println!("输入{guess}");
+    loop {
+        println!("猜猜看");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("无法读取行");
+        //类型转换 str->u32 trim()去除前后空格或者换行，parse()根据:转换成：后面的类型expect错误处理
+        let guess: u32 = guess.trim().parse().expect("请输入数字！");
+        println!("输入{guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("🤷‍♂️太小了🤷‍♂️"),
+            Ordering::Greater => println!("🤦‍♂️太大了🤦‍"),
+            Ordering::Equal => {
+
+                break;
+            }
+        }
+    }
+    println!("恭喜你🎇🎇🎇🎇🎇🎇🎇🎇猜对了🤣🤣");
 }
