27d3dbc5a7bbc16fe1f3e649d89a0e376c7e0e54
ethan shi <ethan.shi@gempoll.com>
2024-11-01 20:35:23 +0800

    使用组件结构关联数据

---
+++ 5.scr结构体组织关联数据/main.rs
@@ -0,0 +1,52 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+// //专属方法 和python的类方法一样
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+// }
+//
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         // 调用方法
+//         // The area of the rectangle is 1500 square pixels.
+//         rect1.area()
+//     );
+// }
+
+// 进阶----------------------------------------------------------------------------------
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2)); //true
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3)); //false
+}
---
+++ 5.scr结构体组织关联数据/例子.rs
@@ -0,0 +1,44 @@
+//计算面积
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
+//
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+//
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// 元组重构
+// fn main() {
+//     let rect1 = (30, 50);
+//
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(rect1)
+//     );
+// }
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+// 使用结构体重构：赋予更多意义
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let aa = area(&rect1);
+    println!("计算出的面积是{aa}", );
+}
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
